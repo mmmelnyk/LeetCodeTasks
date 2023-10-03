72c10d33896e350f1d924fb2070f7553bfa2feea
Melnyk Mykola <kolanio757@gmail.com>
2023-10-03 13:21:35 +0300

    init repo

---
+++ testTasks/LeetCodeTop150Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace testTasks
+{
+	public class LeetCodeTop150Solution
+	{
+		public LeetCodeTop150Solution()
+		{
+		}
+
+        ///189. Rotate Array
+        public void Rotate(int[] nums, int k)
+        {
+            var numsCopy = (int[])nums.Clone();
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (j + k < nums.Length)
+                {
+                    nums[j + k] = numsCopy[j];
+                }
+                else
+                {
+                    nums[(j + k) % nums.Length] = numsCopy[j];
+                }
+            }
+        }
+    }
+}
+
---
+++ testTasks/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//var nums = new int[] { 3, 2, 2, 3 };
+//var val = 3;
+
+//int valCounter = nums.Length;
+//nums = nums.Where(c => c != val).ToArray();
+//valCounter -= nums.Length;
+//Console.WriteLine(valCounter);
+//Array.ForEach(nums, Console.WriteLine);
+var k = 3;
+var nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+for (var j = 0; j < nums.Length; j++)
+{
+    if (j + k < nums.Length)
+    {
+        nums[j + k] = nums[j];
+    }
+    else
+    {
+        nums[nums.Length - (j + k + 1)] = nums[j];
+    }
+
+}
+
+Array.ForEach(nums, Console.WriteLine);
